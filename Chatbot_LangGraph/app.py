dbdb702e866ce8c876e59f0d32c552b4313925d3
hashimaawan <awanhashim800@gmail.com>
2025-08-22 17:35:46 +0500

    Added frontend chatbot

--- Chatbot_LangGraph/app.py
+++ Chatbot_LangGraph/app.py
@@ -16,58 +16,30 @@ llm = ChatGoogleGenerativeAI(
     max_retries=2,
     
 )
-#Testing the LLM
-#print(llm.invoke("Capital of france?"))
+
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
 
 
-class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
+class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
-def chat_llm(state : State):
-    return {"messages": llm.invoke(state["messages"][-1]["content"])}
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
 
+# Checkpointer
+checkpointer = InMemorySaver()
 
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
 
-graph_builder = StateGraph(State)
-
-#adding nodes to the graph
-
-graph_builder.add_node("chat_node", chat_llm)
-
-graph_builder.add_edge(START, "chat_node")
-graph_builder.add_edge("chat_node", END)
-
-graph = graph_builder.compile()
-
-
-### VISUALIZE THE GRAPH
-# from IPython.display import Image, display
-
-# try:
-#     display(Image(graph.get_graph().draw_mermaid_png()))
-# except Exception:
-#     # This requires some extra dependencies and is optional
-#     pass
-
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-
-
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+chatbot = graph.compile(checkpointer=checkpointer)
